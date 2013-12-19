f4afd5f6812ac49dc6722f406f7011c13d3dbec2
Kyle Wickens <lonewolfkid@gmx.com>
2013-12-19 10:17:20 +0000

    tutorial 13

---
+++ 13-MyEnhancedForLoop/MyEnhancedForLoop.java
@@ -0,0 +1,10 @@
+public class MyEnhancedForLoop {
+
+public static void main(String args[]) {
+	int[] numbers = { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 };
+
+	for (int i : numbers) {
+		System.out.println("i:" + i);
+	}
+}
+}
