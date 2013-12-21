809808da690838e90d2bbe5c607030a5287f7aa4
Kyle Wickens <lonewolfkid@gmx.com>
2013-12-21 08:36:41 +0000

    tutorial 15

---
+++ 15-MyIfElseStatement/MyIfElseStatement.java
@@ -0,0 +1,14 @@
+public class MyIfElseStatement {
+
+public static void main(String args[]) {
+	int i = 45;
+
+	if (i < 30) {
+		System.out.println("if statement has been triggered");
+	} else if (i < 40) {
+		System.out.println("else if statement has been triggered");
+	} else {
+		System.out.println("else statement has been triggered");
+	}
+}
+}
