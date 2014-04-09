70b25b78347f7586d3aaf9c77c5f429c7f23b46c
Kyle Wickens <kylewickens@gmail.com>
2014-04-09 20:41:21 +0100

    tutorial 26

---
+++ 26-MyMethodsOverloading/MyMethodsOverloadDemo.java
@@ -0,0 +1,40 @@
+/***************************************************************************
+*
+* Project: MyJavaTutorials
+*
+* Copyright (c) 2012 - 2014, Kyle Wickens, <kylewickens@gmail.com>, et al.
+*
+* This software is licensed as described in the file COPYING, which you
+* should have received as part of this distribution. The terms are also
+* available at http://kylewickens.blogspot.co.uk/p/copyright-license.html.
+*
+* You may opt to use, copy, modify, merge, publish, distribute and/or sell
+* copies of the Software, and permit persons to whom the Software is
+* furnished to do so, under the terms of the COPYING file.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+***************************************************************************/
+public class MyMethodsOverloadDemo {
+
+public static int min(int x, int y) {
+	return x < y ? x : y;
+}
+
+public static double min(double x, double y) {
+	return x < y ? x : y;
+}
+
+public static int min(int w, int x, int y, int z) {
+	return min(min(w, x), min(y, z));
+}
+
+public static double min(double w, double x, double y, double z) {
+	return min(min(w, x), min(y, z));
+}
+
+public static void main(String[] args) {
+	System.out.println("Minimum is " + min(4.5, 3.3, 2.7, 1.4));
+}
+}
