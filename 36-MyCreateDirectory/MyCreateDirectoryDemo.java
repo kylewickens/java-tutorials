74015b11485a67f631c17ed1b5c2e020ff798cda
Kyle Wickens <kylewickens@gmail.com>
2014-06-04 05:53:42 +0100

    tutorial 36

--- 36-MyCreateDirectory/MyCreateDirectoryDemo.java
+++ 36-MyCreateDirectory/MyCreateDirectoryDemo.java
@@ -17,43 +17,12 @@
 *
 ***************************************************************************/
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.FileNotFoundException;
 
-class MyFileReadWriteDemo {
+class MyCreateDirectoryDemo {
 	public static void main(String[] args){
 		if (args.length == 1) {
 			File f = new File(args[0]);
-
-			try {
-				f.createNewFile();
-			} catch (IOException e) {
-				e.getMessage();
-			}
-
-			try {
-				FileWriter w = new FileWriter(f);
-				w.write("Welcome to kylewickens.blogspot.co.uk\n");
-				w.close();
-			} catch (IOException e) {
-				e.getMessage();
-			}
-
-			try {
-				char[] data = new char[100];
-
-				FileReader r = new FileReader(f);
-				r.read(data);
-				r.close();
-
-				System.out.print(data);
-			} catch (FileNotFoundException e) {
-				e.getMessage();
-			} catch (IOException e) {
-				e.getMessage();
-			}
+			f.mkdirs();
 		}
 	}
 }
