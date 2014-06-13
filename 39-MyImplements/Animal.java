20a33caf55273d4599875040eb654a9bbe92c925
Kyle Wickens <kylewickens@gmail.com>
2014-06-13 19:10:15 +0100

    tutorial 39

---
+++ 39-MyImplements/Animal.java
@@ -0,0 +1,20 @@
+/***************************************************************************
+*
+* Project: MyJavaTutorials
+*
+* Copyright (c) 2012 - 2014, Kyle Wickens, <kylewickens@gmail.com>, et al.
+*
+* This software is licensed as described in the file COPYING, which you
+* should have received as part of this distribution. The terms are also
+* available at http://kylewickens.blogspot.co.uk/p/copyright-license.html.
+*
+* You may opt to use, copy, modify, merge, publish, distribute and/or sell
+* copies of the Software, and permit persons to whom the Software is
+* furnished to do so, under the terms of the COPYING file.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+***************************************************************************/
+public interface Animal {
+}
---
+++ 39-MyImplements/MyImplementsDemo.java
@@ -0,0 +1,27 @@
+/***************************************************************************
+*
+* Project: MyJavaTutorials
+*
+* Copyright (c) 2012 - 2014, Kyle Wickens, <kylewickens@gmail.com>, et al.
+*
+* This software is licensed as described in the file COPYING, which you
+* should have received as part of this distribution. The terms are also
+* available at http://kylewickens.blogspot.co.uk/p/copyright-license.html.
+*
+* You may opt to use, copy, modify, merge, publish, distribute and/or sell
+* copies of the Software, and permit persons to whom the Software is
+* furnished to do so, under the terms of the COPYING file.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+***************************************************************************/
+class MyImplementsDemo {
+	public static void main(String[] args){
+		Dog d = new Dog();
+		Bulldog b = new Bulldog();
+
+		System.out.println("Dog IS-A Animal: " + (d instanceof Animal));
+		System.out.println("Bulldog IS-A Dog: " + (b instanceof Dog));
+	}
+}
