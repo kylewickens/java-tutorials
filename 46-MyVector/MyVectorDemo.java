9b893ec8a5ea24f248930f6e132c253f6f7d1c19
Kyle Wickens <kylewickens@gmail.com>
2014-07-02 06:26:39 +0100

    tutorial 46

---
+++ 46-MyVector/MyVectorDemo.java
@@ -0,0 +1,42 @@
+/***************************************************************************
+*
+* Project: MyJavaTutorials
+*
+* Copyright (c) 2012 - 2014, Kyle Wickens, <kylewickens@gmail.com>, et al.
+*
+* This software is licensed as described in the file COPYING, which you
+* should have received as part of this distribution. The terms are also
+* available at http://kylewickens.blogspot.co.uk/p/copyright-license.html.
+*
+* You may opt to use, copy, modify, merge, publish, distribute and/or sell
+* copies of the Software, and permit persons to whom the Software is
+* furnished to do so, under the terms of the COPYING file.
+*
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+* KIND, either express or implied.
+*
+***************************************************************************/
+import java.util.Vector;
+import java.util.Enumeration;
+
+class MyVectorDemo {
+	public static void main(String[] args) {
+		Vector<String> dogBreeds = new Vector<String>();
+
+		dogBreeds.add("Beagle");
+		dogBreeds.add("Bulldog");
+		dogBreeds.add("Boxer");
+		dogBreeds.add("Dachshund");
+		dogBreeds.add("Rottweiler");
+		dogBreeds.add("Pug");
+		dogBreeds.add("Schipperke");
+		dogBreeds.add("Beauceron");
+		dogBreeds.add("Kuvasz");
+		dogBreeds.add("Landseer");
+
+		Enumeration dogs = dogBreeds.elements();
+		while (dogs.hasMoreElements()) {
+			System.out.println(dogs.nextElement());
+		}
+	}
+}
